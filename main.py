72f0d6e3d3bc2909a032c30f939ce0b75ded2807
Edwin Abot <edwinabot@gmail.com>
2017-10-27 12:15:23 -0300

    some syntax showcase

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+This module contains a brief showcase of Python
+"""
+from showcase import  show_me_some_syntax
+
+
+if __name__ == '__main__':
+
+    print('Hey Sparker! welcome to Python.')
+    print('Now we will call the show_me_some_syntax function.')
+    print('Checkout the code on showcase.py or use the debugger.')
+
+    show_me_some_syntax()
--- showcase.py
+++ showcase.py
@@ -1,6 +1,51 @@
+# pylint: disable=unused-variable
+
 """
-This module contains a brief showcase of Python
+This is the docstring for this module
 """
 
-if __name__ == '__main__':
-    print('Hey Sparker! welcome to Python')
+from datetime import date
+
+
+def show_me_some_syntax():
+    """
+    The first comment is the docstring for this function
+    """
+
+    # This is a one line comment
+
+    '''
+    This is
+    a multiline comment
+    '''
+
+    an_int = 10
+    a_float = 25.32
+    a_string = "Jane Doe"
+    an_interpolated_string = f"The name is: {a_string}"
+    a_date = date(1990, 10, 25)
+    an_object = object()
+    a_list = [1, 5, 25, -58]    # We can access an item by its index: a_list[2]
+    a_tuple = (1, 5, 25, -58)   # the same can be done with a tuple: a_tuple[2]
+    a_dictionary = {'name': a_string, 'birthday': a_date, 'number_of_freckles': an_int}
+                                # A dict's item by it's key value: a_dictionary['name']
+
+    # Lets iterate over a list with a for loop
+    print('\n=== Looping a list [] with a for loop ===')
+    for item in a_list:
+        print(f"The list item: {item}")
+
+    # Lets iterate some over a tuple with a while loop
+    # We could have used a for loop too
+    print('\n=== Looping a tuple () with a while loop ===')
+    counter = 0
+    while counter < len(a_tuple):
+        the_value = a_tuple[counter]
+        print(f"The tuple item: {the_value}")
+        counter += 1
+
+    # Now lets iterate over a dict
+    print('\n=== Looping a dict {} with a for loop ===')
+    for key, value in a_dictionary.items():
+        clean_key = key.replace('_', ' ')  # Let's replace underscores for spaces
+        print(f"The {clean_key} is {value}")
